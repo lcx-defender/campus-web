5126a791b30b196593d38e4209b62ad61b28878d
lcx_defender <1540347109@qq.com>
2025-05-05 13:49:36 +0800

    当前版本已完全能够处理动态路由，尚需解决生成菜单的页面与实际页面不一致的问题

--- src/router/permission.js
+++ src/router/permission.js
@@ -12,12 +12,12 @@ NProgress.configure({ showSpinner: false });
 const whiteList = ['/login', '/', '/index', '/apply', '/schools', '/about']; // no redirect whitelist
 
 router.beforeEach(async (to, from, next) => {
-  console.log('路由跳转', to.path, from.path)
+  console.log('路由跳转', from.path, '-->', to.path)
   NProgress.start()
   const userStore = useUserStore();
   const permissionStore = usePermissionStore();
   if (getToken()) {
-    /* has token*/
+    /* 带有token */
     if (to.path === '/login') {
       console.log('携带token访问登录页,跳转')
       next({ path: '/service' })
@@ -27,15 +27,17 @@ router.beforeEach(async (to, from, next) => {
       console.log('免登录白名单，直接进入', to.path)
       next()
     } else {
-      console.log('携带token访问其他路由', to.path)
+      console.log('携带token访问其他路由',from.path, '-->', to.path)
       // 访问需要token的路由
       // 如果用户信息不存在，说明是第一次访问，获取用户信息和路由信息
+      console.warn('用户信息是否存在:', (userStore.userInfo), ' 路由信息是否存在:', permissionStore.hasRoutes)
       if(!userStore.userInfo && !permissionStore.hasRoutes) {
         // 生成路由,用户信息交给layout组件处理
         try {
           const sidebarRouters = await permissionStore.generateRoutes();
           console.log('路由守卫router',router.getRoutes())
           console.log('路由守卫sidebarRouters', sidebarRouters)
+          next({ ...to, replace: true }); // 重新导航到目标路径
         } catch (err) {
           console.error('路由加载失败:', err);
           logout();
@@ -44,8 +46,10 @@ router.beforeEach(async (to, from, next) => {
           NProgress.done();
           return; // 确保退出逻辑
         }
+      } else {
+        next();
       }
-      next();
+      
     }
   } else {
     // 没有token
